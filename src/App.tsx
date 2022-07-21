683f816c6999bd0718294d16d86a87e110372275
onoff-jonghan-kim <jonghan.kim@onoff.ne.jp>
2022-07-21 11:33:10 +0900

    #7.3 fin

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,73 @@
-import { useRecoilState, useRecoilValue } from "recoil";
-import { hourSelector, minuteState } from "./atoms";
+import styled from "styled-components";
+import { motion } from "framer-motion";
+
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const Box = styled(motion.div)`
+  width: 200px;
+  height: 200px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  background-color: rgba(255,255,255, 0.2);
+  border-radius: 40px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const Circle = styled(motion.div)`
+  background-color: white;
+  height: 70px;
+  width: 70px;
+  place-self: center;
+  border-radius: 35px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const boxVariants = {
+  start: {
+    opacity:0,
+    scale:0.5,
+  },
+  end: {
+    scale: 1,
+    opacity: 1,
+    transition: {
+      type: "spring",
+      duration: 0.5,
+      bounce: 0.5,
+      delayChildren: 0.2,
+      staggerChildren: 0.1,
+    }
+  }
+}
+
+const circleVariants = {
+  start: {
+    opacity: 0,
+    y: 10,
+  },
+  end: {
+    opacity: 1,
+    y: 0,
+  }
+}
 
 function App() {
-  const [minutes, setMinutes] = useRecoilState(minuteState);
-  const hours = useRecoilValue(hourSelector);
-  const onMinuteChange = (event:React.FormEvent<HTMLInputElement>) => {
-    setMinutes(+event.currentTarget.value);
-  };
   return (
     <>
-      <input type="number" placeholder="Minutes" onChange={onMinuteChange} value={minutes}/>
-      <input type="number" placeholder="Hours" value={hours}/>
+      <Wrapper>
+        <Box variants={boxVariants} initial="start" animate="end">
+          <Circle variants={circleVariants}></Circle>
+          <Circle variants={circleVariants}></Circle>
+          <Circle variants={circleVariants}></Circle>
+          <Circle variants={circleVariants}></Circle>
+        </Box>
+      </Wrapper>
     </>
   );
-}
+};
 
 export default App;
