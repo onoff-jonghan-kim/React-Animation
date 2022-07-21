5bb373b0a0aa74125a011e2ba6a0bcd3804f323d
onoff-jonghan-kim <jonghan.kim@onoff.ne.jp>
2022-07-21 13:58:22 +0900

    #7.5 fin

--- src/App.tsx
+++ src/App.tsx
@@ -18,13 +18,14 @@ const Box = styled(motion.div)`
 const boxVariants = {
   hover: {scale: 1.5, rotateZ: 90},
   click: { scale:1, borderRadius: "100px" },
-}
+  drag: { backgroundColor: "rgb(46, 204, 113)", transition: { duration: 10 } },
+};
 
 function App() {
   return (
     <>
       <Wrapper>
-        <Box variants={boxVariants} whileHover="hover" whileTap="click"> </Box>
+        <Box drag variants={boxVariants} whileHover="hover" whileTap="click" whileDrag="drag"> </Box>
       </Wrapper>
     </>
   );
