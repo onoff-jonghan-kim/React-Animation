7ab57126e5c729b530e9fd35ef91f0169a800c7e
onoff-jonghan-kim <jonghan.kim@onoff.ne.jp>
2022-07-21 14:21:22 +0900

    #7.6 fin

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { motion } from "framer-motion";
+import { useRef } from "react";
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -7,6 +8,19 @@ const Wrapper = styled.div`
   justify-content: center;
   align-items: center;
 `;
+
+const BiggerBox = styled.div`
+  width: 600px;
+  height: 600px;
+  background-color: rgba(255,255,255, 0.7);
+  border-radius: 40px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+`;
+
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
@@ -22,10 +36,13 @@ const boxVariants = {
 };
 
 function App() {
+  const biggerBoxRef = useRef(null);
   return (
     <>
       <Wrapper>
-        <Box drag variants={boxVariants} whileHover="hover" whileTap="click" whileDrag="drag"> </Box>
+        <BiggerBox ref={biggerBoxRef}>
+          <Box drag dragSnapToOrigin dragElastic={0} dragConstraints={biggerBoxRef}  variants={boxVariants} whileHover="hover" whileTap="click" whileDrag="drag"> </Box>
+        </BiggerBox>
       </Wrapper>
     </>
   );
