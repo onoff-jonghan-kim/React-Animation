485ab5b162d23181e8783e03d125dbc3c5f499c5
onoff-jonghan-kim <jonghan.kim@onoff.ne.jp>
2022-07-21 10:32:56 +0900

    #7.0 fin

--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,3 @@
 import { DefaultTheme } from "styled-components";
 
-export const darkTheme: DefaultTheme = {
-  bgColor: "#535c68",
-  textColor: "#ffff",
-  accentColor: "#9c88ff",
-}
+export const darkTheme: DefaultTheme = { };
